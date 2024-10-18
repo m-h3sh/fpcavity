3d1cdbda31db3cf0997b3907eddff31c4fb48b59
maheshgondi <hesh.gondi@gmail.com>
2024-10-18 08:55:15 +0530

    test

---
+++ basic_cavity.py
@@ -0,0 +1,10 @@
+# Importing the libraries
+import finesse
+from finesse.components import Laser, Gauss, Mirror
+from finesse.components.space import Space
+from finesse.components.beamsplitter import Beamsplitter
+from finesse.components.cavity import Cavity
+from finesse.components.detector import PowerDetector
+from finesse.analysis.actions.axes import Xaxis
+import numpy as np
+import matplotlib.pyplot as plt
