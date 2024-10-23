1bf261b025215ed5fd7fdd791706b2230935e831
maheshgondi <hesh.gondi@gmail.com>
2024-10-23 14:52:20 +0530

    basic structure

--- basic_cavity.py
+++ basic_cavity.py
@@ -13,30 +13,34 @@ import matplotlib.pyplot as plt
 
 # Defining variables
 wavelength = 1064e-9
-cav_len = 0.5
+cav_len = 0.49999967 # resonant length for wavelength 1064 nm
 
 # Defining the model
 model = finesse.Model()
-model.lambda0 = wavelength
+# model.lambda0 = wavelength
 
 # Adding laser source
-laser = Laser("source", 1.0, 2.82e14)
+laser = Laser("source", 1.0, 2.8167763157e14)
 model.add(laser)
 
 # Adding Gaussian parameters to the beam
-w0 = np.sqrt(model.lambda0 * ((93.15 + 26)*1e-3 / 2) / np.pi)
+w0 = np.sqrt((3e8/laser.f) * ((93.15 + 26)*1e-3 / 2) / np.pi)
 glaser = Gauss("glaser", laser.p1.o, w0=w0, z=0)
 model.add(glaser)
 
 # Defining the mirrors
-mx1 = Mirror("mx1", R=0.99, T=0.01)
-mx2 = Mirror("mx2", R=0.99, T=0.01)
-my1 = Mirror("my1", R=0.99, T=0.01)
-my2 = Mirror("my2", R=0.99, T=0.01)
+mx1 = Mirror("mx1", R=0.99, L=0.0)
+mx2 = Mirror("mx2", R=0.999, T=0.001)
+my1 = Mirror("my1", R=0.99, L=0.0)
+my2 = Mirror("my2", R=0.999, T=0.001)
 model.add(mx1)
 model.add(mx2)
 model.add(my1)
 model.add(my2)
+
+# Adding 6 dof's to the mirrors
+# In finesse the optical path is along z axis
+
 
 # Defining the beamsplitter
 bs = Beamsplitter("bs", 0.5, 0.5, alpha=45.0)
@@ -58,13 +62,21 @@ model.add(Cavity("xcav", mx1.p2.o))
 model.add(Cavity("ycav", my1.p2.o))
 
 xaxis = Xaxis(mx1.phi, 'lin', -180 , 180, 400)
+# xaxis = Xaxis(laser.f, 'lin', 2.44e14, 2.84e14, 300)
+# xaxis = Xaxis(x2.L, 'lin', 0.5, 0.6, 200)
 output = model.run(xaxis)
 
 print(output)
 plt.figure("Analyzing Output")
 x = np.linspace(-180, 180, 401)
-plt.plot(x, output["pd"], label="Output power")
+plt.plot(x, output["pd"], label="Output Power")
 plt.xticks(np.arange(-180, 180, step=50))
 plt.xlabel("angle (phi)")
 plt.yscale('log')
 plt.show()
+
+
+# plt.plot(output["pd"], label="Output Power")
+# plt.ylabel("Output power (W)")
+# plt.yscale("log")
+# plt.show()
